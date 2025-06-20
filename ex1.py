1a302774152b77e08c953e17e61e4e4301683657
IsharaKarunarathne <tmihk1995@gmail.com>
2025-06-20 15:12:18 +0530

    first commit

---
+++ ex1.py
@@ -0,0 +1,15 @@
+#create the list named temperatures
+temperatures = [25.5, 28.1, 22.9, 30.2, 27.8, 24.3, 29.5]
+
+#add 31.0 to the end of the list
+temperatures.append(31.0)
+
+#remove the third element from the list
+temp=temperatures.pop(2)
+print(temp)
+
+print(f"modified_list: {temperatures}")
+max_temperature = max(temperatures)
+print(f"max temp: {max_temperature}")
+min_temperature = min(temperatures)
+print(f"min temp: {min_temperature}")
